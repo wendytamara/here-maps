13bbd5e8342119dad351dee15778e141d665953d
Jessica Meza <jessicamezarojas2011@gmail.com>
2018-04-01 14:46:30 -0500

    identacion

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,19 @@
 var platform = new H.service.Platform({
   'app_id': 'dtSI2axPe5QEr4QN4Bmd',
   'app_code': 'cciCXXxEAcSyxCtSkOFyZg'
-  });
+});
   
   // Obtain the default map types from the platform object
-  var maptypes = platform.createDefaultLayers();
+var maptypes = platform.createDefaultLayers();
   
-  // Instantiate (and display) a map object:
-  var map = new H.Map(
+// Instantiate (and display) a map object:
+var map = new H.Map(
   document.getElementById('mapContainer'),
   maptypes.normal.map,
   {
-  zoom: 10,
-  center: { lat: -12.0463731, lng: -77.042754 }
+    zoom: 10,
+    center: { 
+      lat: -12.0463731,
+      lng: -77.042754 
+    }
   });
