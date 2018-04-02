5890196f2f3752da295d6bf321e32fbe2b762ff0
wendytamara <anacarlavegam@gmail.com>
2018-04-02 07:50:06 -0500

    añadiendo funcion de filtrado de lugares

--- public/js/app.js
+++ public/js/app.js
@@ -27,40 +27,155 @@
     })
     
 
-  
-    function moveMapToBerlin(map){
-      map.setCenter({lat:-12.0566, lng:-76.961});
-      map.setZoom(14);
+  // Instantiate the Platform class with authentication and
+// authorization credentials:
+
+var campText = document.getElementById('camp-text');
+
+function moveMapToBerlin(map){
+  map.setCenter({lat:-12.0463731, lng:-77.042754});
+  map.setZoom(14);
+}
+
+var platform = new H.service.Platform({
+  app_id: 'dtSI2axPe5QEr4QN4Bmd',
+  app_code: 'cciCXXxEAcSyxCtSkOFyZg',
+  useCIT: true,
+  useHTTPS: true
+});
+
+var defaultLayers = platform.createDefaultLayers();
+
+// Crear una instancia de un mapa dentro del elemento DOM con id map. los
+// map center está en San Francisco, el nivel de zoom es 10:
+var map = new H.Map(document.getElementById('map'),
+defaultLayers.normal.map);
+
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+
+// Crea un objeto de grupo para contener marcadores de mapa:
+var group = new H.map.Group();
+
+// Crea los componentes de UI predeterminados:
+var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
+
+// Add the group object to the map:
+map.addObject(group);
+
+// Obtener un objeto de búsqueda a través del cual enviar la búsqueda
+   // peticiones:
+var search = new H.places.Search(platform.getPlacesService()),
+
+searchResult, error;
+
+var btnSearch = document.getElementById('btnSearch');
+var campText = document.getElementById('camp-text');
+btnSearch.addEventListener('click', capturingValueInput);
+// campText.addEventListener('keyup', capturingValueInput);
+
+var input = '';
+function capturingValueInput(event) {
+  event.preventDefault();
+  input = campText.value;
+  console.log(input);
+  var params = {
+// Búsqueda de texto simple para lugares con la palabra "hotel"
+     // asociado a ellos:
+  'q': input,
+ // Buscar en el distrito de Chinatown en San Francisco:
+  'at': '-12.0463731,-77.042754'
+  };
+
+    search.request(params, {}, onResult, onError);
+}
+
+
+// capturingValueInput();
+
+// Define search parameters:
+
+
+// Define una función de devolución de llamada para manejar datos en caso de éxito:
+
+function onResult(data) {
+addPlacesToMap(data.results);
+showTemplate(data.results);
+console.log(data.results);
+}
+
+// Define a callback function to handle errors:
+function onError(data) {
+error = data;
+}
+
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+// Now use the map as required...
+moveMapToBerlin(map);
+
+// Esta función agrega marcadores al mapa, indicando cada uno de
+     // los lugares ubicados
+
+function addPlacesToMap(result) {
+  // console.log(result)
+  // console.log(result.items)
+
+group.addObjects(result.items.map(function (place) {
+var marker = new H.map.Marker({lat: place.position[0],
+lng: place.position[1]})
+return marker;
+
+}));
+}
+
+
+
+// Run a search request with parameters, headers (empty), and
+// callback functions:
+
+
+
+
+      var templateCard = $('#templateCard');
+
+    function showTemplate(result){
+       templateCard.empty();
+      console.log(result);
+      var resultado = result.items;
+
+
+      var placesItems = resultado.map(function (places, index) {
+
+
+           console.log(places.title)
+           var template = ` <div class="row">
+             <div class="col s12">
+               <div class="card blue-grey darken-1">
+                 <div class="card-content white-text">
+                   <span class="card-title">${places.title}</span>
+                   <p>${places.vicinity}</p>
+                 </div>
+                 <div class="card-action">
+                   <a href="#">This is a link</a>
+                   <a href="#">This is a link</a>
+                 </div>
+               </div>
+             </div>
+           </div>`;
+
+           templateCard.append(template);
+
+       });
+
+
+
     }
-    
-    
-    /**
-     * Boilerplate map initialization code starts below:
-     */
-    
-    //Step 1: initialize communication with the platform
-    var platform = new H.service.Platform({
-      app_id: 'dtSI2axPe5QEr4QN4Bmd',
-      app_code: 'cciCXXxEAcSyxCtSkOFyZg',
-      useCIT: true,
-      useHTTPS: true
-    });
-    var defaultLayers = platform.createDefaultLayers();
-    
-    //Step 2: initialize a map  - not specificing a location will give a whole world view.
-    var map = new H.Map(document.getElementById('map'),
-      defaultLayers.normal.map);
-    
-    //Step 3: make the map interactive
-    // MapEvents enables the event system
-    // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-    var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-    
-    // Create the default UI components
-    var ui = H.ui.UI.createDefault(map, defaultLayers);
-    
-    // Now use the map as required...
-    moveMapToBerlin(map);
+
+
+
+
+
 
   }
 
