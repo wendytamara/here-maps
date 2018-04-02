ff5424b4ad6a24d8a6802fab6ace9845717b0ca2
wendytamara <anacarlavegam@gmail.com>
2018-04-01 19:06:51 -0500

    añadiendo geolocalizacion

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,11 @@
-// Initialize the platform object:
-var platform = new H.service.Platform({
-  'app_id': 'dtSI2axPe5QEr4QN4Bmd',
-  'app_code': 'cciCXXxEAcSyxCtSkOFyZg'
-  });
-  
-  // Obtain the default map types from the platform object
-  var maptypes = platform.createDefaultLayers();
-  
-  // Instantiate (and display) a map object:
-  var map = new H.Map(
-  document.getElementById('mapContainer'),
-  maptypes.normal.map,
-  {
-  zoom: 11,
-  center: { lat: -12.0463731, lng: -77.042754 }
-  });
+  // API DE HTML 5 GEOLOCATION
 
   var x = document.getElementById("demo");
+  var btn = document.getElementById("btn");
   
-  function getLocation() {
+  window.onload = function getLocation() {
+    var latitud = '';
+    var longitud = '';
       if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(showPosition);
       } else { 
@@ -26,7 +13,44 @@ var platform = new H.service.Platform({
       }
   }
   
+  
   function showPosition(position) {
-      x.innerHTML = "Latitude: " + position.coords.latitude + 
-      "<br>Longitude: " + position.coords.longitude;
+
+    latitud = position.coords.latitude;
+    longitud = position.coords.longitude;
+  
+    btn.addEventListener('click', function(){
+      x.value = position.coords.latitude + ' ' + position.coords.longitude;
+    })
+    
+
+      var platform = new H.service.Platform({
+        'app_id': 'dtSI2axPe5QEr4QN4Bmd',
+        'app_code': 'cciCXXxEAcSyxCtSkOFyZg'
+        });
+        
+        // Obtain the default map types from the platform object
+        var maptypes = platform.createDefaultLayers();
+        
+        // Instantiate (and display) a map object:
+        var map = new H.Map(
+        document.getElementById('mapContainer'),
+        maptypes.normal.map,
+        {
+        zoom: 11,
+        center: { lat: latitud, lng: longitud }
+        });
+
+        console.log(latitud);
+        console.log(longitud);
   }
+
+  
+  // console.log(latitud);
+  // console.log(longitud);
+
+// Initialize the platform object:
+
+
+
+
